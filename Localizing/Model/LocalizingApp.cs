03ef9014669c6686884e5206faf75628564c8f3f
Макс Титов <hhit934@gmail.com>
2022-09-24 23:13:12 +0300

    Добавьте файлы проекта.

---
+++ Localizing/Model/LocalizingApp.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Localizing.Model
+
+{
+    public class SetLocalized
+    {
+        public SetLocalized()
+        {
+           
+        }
+        public void Set( string name)
+        {
+            string path_ = "DataResource";
+            ResourceDictionary dict = new ResourceDictionary();
+            dict.Source = new Uri(Path.Combine(path_ , name), UriKind.RelativeOrAbsolute);
+            ResourceDictionary oldDict = (from d in Application.Current.Resources.MergedDictionaries
+                                          where d.Source != null select d).First();
+            int ind = Application.Current.Resources.MergedDictionaries.IndexOf(oldDict);
+            Application.Current.Resources.MergedDictionaries.Remove(oldDict);
+            Application.Current.Resources.MergedDictionaries.Insert(ind, dict);
+        }
+    }
+}
---
+++ Localizing/View/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using Localizing.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Localizing
+{
+  
+    public partial class MainWindow : Window
+    {
+        SetLocalized localized = new SetLocalized();
+        bool b = false;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ReUpdate();
+        }
+        void ReUpdate()
+        {
+            b = !b;
+            localized.Set(b == true ? "Rus.xaml" : "Eng.xaml");
+        }
+    }
+}
